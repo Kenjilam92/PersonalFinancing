a88c23f0a5b1e19768b58d524d6b77b23608282c
Trong Ho <74184@hexaware.com>
2022-06-16 09:46:19 -0400

    update from main branch

--- src/main/java/Repository/RepositoryWallet.java
+++ src/main/java/Repository/RepositoryWallet.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//@Transactional
+@Transactional
 public class RepositoryWallet {
 	@Autowired
 	EntityManager em;
